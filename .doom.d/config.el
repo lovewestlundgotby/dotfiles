9253e5dfa3128a16d85a1b0430b47c02eb96fa34
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2020-01-01 12:31:23 +0100

    Doom: Add doom emacs config

---
+++ .doom.d/config.el
@@ -0,0 +1,8 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+;; Decrease delay until which-key pops up.
+(setq which-key-idle-delay 0.4)
+
+(setq doom-font (font-spec :family "Consolas NF" :size 14))
