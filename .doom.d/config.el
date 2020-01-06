2234a3a6188ad3884d4baa03c32d9790fc286d81
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2020-01-06 22:03:10 +0100

    doom: Add clang-format keybind

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,3 +6,7 @@
 (setq which-key-idle-delay 0.4)
 
 (setq doom-font (font-spec :family "Consolas NF" :size 14))
+
+;; Press ctrl+alt+tab to format region with clang-format
+(load "/usr/share/clang/clang-format.el")
+(global-set-key [C-M-tab] 'clang-format-region)
