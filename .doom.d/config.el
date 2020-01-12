b1cca25e5525d999e005cd65eb13c63f549e8d7c
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2020-01-12 21:58:50 +0100

    doom: Disable line numbers

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -5,6 +5,9 @@
 ;; Decrease delay until which-key pops up.
 (setq which-key-idle-delay 0.4)
 
+;; (setq display-line-numbers-type 'relative)
+(setq display-line-numbers-type nil)
+
 (setq doom-font (font-spec :family "Consolas NF" :size 14))
 
 ;; Press ctrl+alt+tab to format region with clang-format
