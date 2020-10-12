c4c030111eb1506c78bf76f85fd8cef01e5d6c81
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2020-10-12 17:14:19 +0200

    zsh: Update and clean abbreviations

--- .config/zsh/abbreviations.zsh
+++ .config/zsh/abbreviations.zsh
@@ -68,41 +68,40 @@ abbrevs+=(
 abbrevs+=(
   "gs"    "git status -sb"
   "gsl"   "git status"
-  "g1"   "git log1"
-  "g2"   "git log2"
+  "g1"    "git log1"
+  "g2"    "git log2"
+  "g3"    "git log3"
 
-  "ga"   "git add"
-  "gad"  "git add ."
-  "gau"  "git add -u"
-  "gap"  "git add -p"
+  "ga"    "git add"
+  "gad"   "git add ."
+  "gau"   "git add -u"
+  "gap"   "git add -p"
 
   "gc"    "git commit -v"
   "gcf"   "git commit --fixup"
   "gca"   "git commit -v --amend"
   "gcane" "git commit --amend --no-edit"
 
-  "gco"     "git checkout"
-  "gcm"    "git checkout master"
-  "gcd"    "git checkout develop"
-  "gcopr"   'git checkout $(git pulls list | grep "^__CURSOR__" | sed -n "s/^\(.*\):\(.*\)$/\2/p")'
-  "gcoh"    "git checkout HEAD"
-  "gcohd"   "git checkout HEAD --"
-  "gcl"     "git clone"
-  "gclc"    "git clone __CURSOR__ && cd \$(basename \$_)"
-  "gb"      "git branch"
-  "gbm"     "git branch -M"
-  "gbv"     "git branch -vv"
-  "gba"     "git branch -a"
-  "gbav"    "git branch -a -vv"
-  "gbda"   "git fetch -p && for branch in \$(git branch -vv | grep ': gone]' | awk '{print \$1}'); do git branch -D \$branch; done"
+  "gco"   "git checkout"
+  "gcm"   "git checkout master"
+  "gcd"   "git checkout develop"
+  "gcl"   "git clone"
+  "gb"    "git branch"
+  "gbm"   "git branch -M"
+  "gbv"   "git branch -vv"
+  "gba"   "git branch -a"
+  "gbav"  "git branch -a -vv"
+  "gbda"  "git fetch -p && for branch in \$(git branch -vv | grep ': gone]' | awk '{print \$1}'); do git branch -D \$branch; done"
 
-  # "gbmd"   'git branch --merged | grep  -v "\*\|master" | xargs -n1 git branch -d'
-  # "gbrmd"  'git branch -r --merged | grep origin | grep -v "\->\|master" | cut -d"/" -f2- | xargs git push origin --delete'
+  # Remove local branches merged to HEAD.
+  "gbdm"   'git branch --merged | grep -v "\*\|master\|develop" | xargs -n1 git branch -d'
+  # Remove remote branches that exist locally and have been merged to HEAD.
+  "gbdmr" 'git branch -r --merged | sed ",origin/,," | xargs -n1 git branch -D 2> /dev/null'
 
   "gd"    "git diff"
   # "gdm"   "git diff origin/master.."
   # "gdms"  "git diff origin/master:./"
-  # "gdc"   "git diff --cached"
+  "gdc"   "git diff --cached"
   "gdt"   "git difftool"
   "gdh"   "git diff HEAD~1"
 
@@ -119,18 +118,6 @@ abbrevs+=(
   "glor"  "git pull origin --rebase"
   # "glomr" "git pull origin master --rebase"
 
-  # "glu"   "git pulls update"
-  # "gll"   "git pulls list"
-  # "gls"   "git pulls show"
-  # "glsf"  "git pulls show __CURSOR__ --full"
-  # "glb"   "git pulls browse"
-  # "glm"   "git pulls merge"
-
-  # "gpr"   "hub pull-request"
-  # "gprne" "EDITOR='vim -c \":wq\"' hub pull-request"
-  # "gprm"  'git log master.. --format="%B" --reverse > .git/PULLREQ_EDITMSG && git push -u && hub pull-request'
-  # "blb"   '-b $(git rev-parse --abbrev-ref @{-1})'
-
   "grb"   "git rebase"
   "grbi"  "git rebase -i"
   "grbia" "git rebase -i --autosquash"
@@ -138,49 +125,41 @@ abbrevs+=(
   "grbc"  "git rebase --continue"
   "grbm"  "git rebase master"
   "grbd"  "git rebase develop"
-  "grbom"  "git rebase origin/master"
+  "grbom" "git rebase origin/master"
   "grbim" "git rebase -i master"
 
   "gr"    "git reset"
   "grh"   "git reset --hard"
-  "grhu"  "git reset --hard @{u}"
-  "grsm"  "git reset --soft master"
+  "grs"   "git reset --soft"
 
   "gchp"  "git cherry-pick"
   "gchpc" "git cherry-pick --continue"
   "gchpa" "git cherry-pick --abort"
 
-  "gsh"  "git show"
-  "gshh" "git show HEAD"
+  "gsh"   "git show"
+  "gshs"  "git show --summary"
+  "gshh"  "git show HEAD"
 
-  "gsu"  "git submodule update --init --recursive"
-  "gss"  "git submodule sync"
-  "gsgl" "git submodule -q foreach git pull -q origin master"
+  "gsu"   "git submodule update --init --recursive"
+  "gss"   "git submodule sync"
+  "gsgl"  "git submodule -q foreach git pull -q origin master"
 
-  "gsta" "git stash push"
-  "gstl" "git stash list"
-  "gstp" "git stash pop"
-  "gstd" "git stash drop"
+  "gsta"  "git stash push"
+  "gstl"  "git stash list"
+  "gstp"  "git stash pop"
+  "gstd"  "git stash drop"
 
   "gcb"   "git checkout -b"
 
   "ggsup" 'git branch --set-upstream-to=origin/$(git branch | grep \* | cut -d " " -f2)'
   "gpsup" 'git push --set-upstream origin $(git branch | grep \* | cut -d " " -f2)'
 
-  "gbd" "git branch -d"
-  "gbD" "git branch -D"
+  "gbd"   "git branch -d"
+  "gbD"   "git branch -D"
 
   # "vgu"  'vim $(git ls-files --unmerged | cut -f2 | sort -u)'
   # "gcdi" "git clean -di"
 )
-
-# Add alias and autocompleteion for hub
-type compdef >/dev/null 2>&1 && compdef hub=git
-type hub >/dev/null 2>&1 && alias git='hub'
-
-for abbr in ${(k)abbrevs}; do
-  # alias $abbr="${abbrevs[$abbr]}"
-done
 
 magic-abbrev-expand() {
   zle expand-dots
