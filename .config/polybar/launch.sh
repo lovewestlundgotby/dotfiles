da62b8ad036901e358686d553b91f06ff4a8b307
Love Westlund Gotby <lovewestlundgotby@gmail.com>
2020-10-14 18:01:56 +0200

    polybar: Update config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Terminate already running bar instances
+# Terminate already running bar instances.
 killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
 
-# Wait until the process has been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bars
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    a=$(polybar --list-monitors | grep "$m" | grep "primary")
+    if [[ -n "$a" ]]; then
+        log=/tmp/polybar_bottom_primary.log
+        MONITOR=$m polybar -r bottom_primary >> $log 2>&1 &
+    else
+        log=/tmp/polybar_bottom_secondary.log
+        MONITOR=$m polybar -r bottom_secondary >> $log 2>&1 &
+    fi
+    echo "---" | tee -a $log
+done
 
-# Launch bar(s)
-polybar bottom &
-
-echo 'Bars launched...'
+echo "Bars launched..."
